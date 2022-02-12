c1873be45a073e9373abfd9be2b18a705088edc5
jihoonchoi.94@gmail.com <jihoonchoi.94@gmail.com>
2022-02-12 10:16:45 -0800

    seeds

--- app/seeds/users.py
+++ app/seeds/users.py
@@ -5,14 +5,11 @@ from app.models import db, User
 def seed_users():
     demo = User(
         username='Demo', email='demo@aa.io', password='password')
-    marnie = User(
-        username='marnie', email='marnie@aa.io', password='password')
-    bobbie = User(
-        username='bobbie', email='bobbie@aa.io', password='password')
+    fiona = User(
+        username='fiona', email='fiona@aa.io', password='password')
 
     db.session.add(demo)
-    db.session.add(marnie)
-    db.session.add(bobbie)
+    db.session.add(fiona)
 
     db.session.commit()
 

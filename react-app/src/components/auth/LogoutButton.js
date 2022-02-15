940b21e5bab064d0eecdab9eaa199d65d9d8904e
jihoonchoi.94@gmail.com <jihoonchoi.94@gmail.com>
2022-02-15 07:54:52 -0800

    history push on logout

--- react-app/src/components/auth/LogoutButton.js
+++ react-app/src/components/auth/LogoutButton.js
@@ -1,11 +1,14 @@
 import React from 'react';
 import { useDispatch } from 'react-redux';
+import { useHistory } from 'react-router-dom';
 import { logout } from '../../store/session';
 
 const LogoutButton = () => {
-  const dispatch = useDispatch()
+  const dispatch = useDispatch();
+  const history = useHistory();
   const onLogout = async (e) => {
     await dispatch(logout());
+    history.push('/');
   };
 
   return <button onClick={onLogout}>Logout</button>;

90ea37fdfd52a402aaaa7a65eac0466870969243
jihoonchoi.94@gmail.com <jihoonchoi.94@gmail.com>
2022-02-16 12:03:49 -0800

    chat styling

--- react-app/src/components/Chat/index.js
+++ react-app/src/components/Chat/index.js
@@ -30,7 +30,7 @@ const Chat = () => {
 
     const sendChat = (e) => {
         e.preventDefault();
-        socket.emit('chat', { user: user.username, msg: chatInput, room: room?.room_name });
+        socket.emit('chat', { user: user.username, msg: chatInput, room: room?.room_name, user_image: user.image });
         dispatch(createChatMessage(roomId, chatInput));
         setChatInput("");
     }
@@ -45,7 +45,7 @@ const Chat = () => {
         socket.emit('join', { 'username': user.username, 'room': room?.room_name })
         console.log(user.username, 'joined room')
 
-        socket.emit('chat', { user: 'weStudy-Bot', msg: `${user.username} has joined the room.`, room: room.room_name })
+        socket.emit('chat', { user: 'weStudy-Bot', msg: `${user.username} has joined the room.`, room: room?.room_name })
 
 
         socket.on('chat', (chat) => {
@@ -54,38 +54,55 @@ const Chat = () => {
 
         return (() => {
             console.log('leaving room')
-            socket.emit('leave', { 'username': user.username, 'room': room.room_name })
-            socket.emit('chat', { user: 'weStudy-Bot', msg: `${user.username} has left the room.`, room: room.room_name })
+            socket.emit('leave', { 'username': user.username, 'room': room?.room_name })
+            socket.emit('chat', { user: 'weStudy-Bot', msg: `${user.username} has left the room.`, room: room?.room_name })
 
             socket.disconnect();
         })
     }, [])
 
     return (
-        <div className='chat-room-container'>
+        // <div className='chat-and-input-container'>
+        <>
+            <div className='chat-room-container'>
                 {chats.map(chat => {
                     return <div
                         className={chat.username === user.username ? 'right chat-msg' : 'left chat-msg'}
                         key={chat.id}>
-                            <div className='profile-pic-div chat-profile-pic'>
-                                <img src={chat.user_image} alt={chat.username}></img>
-                            </div>
-                            <div className='chat-message'>
-                                {chat.message}
-                            </div>
+                        <div className='profile-pic-div chat-profile-pic'>
+                            <img src={chat.user_image} alt={chat.username}></img>
                         </div>
+                        <div className='chat-message'>
+                            {chat.message}
+                        </div>
+                    </div>
                 })}
                 {messages.map((message, idx) => (
-                    <div className={message.user === 'weStudy-Bot' ? 'center chat-msg' : 'chat-msg'} key={idx}>{`${message.user}: ${message.msg}`}</div>
+                    <div
+                        className={message.user === 'weStudy-Bot' ? 'center chat-msg' : message.user === user.username ? 'right chat-msg' : 'left chat-msg'}
+                        key={idx}>
+                        {message.user !== 'weStudy-Bot' &&
+                            <div className='profile-pic-div chat-profile-pic'>
+                                <img src={message.user_image} alt={message.user}></img>
+                            </div>
+                        }
+                        <div className='chat-message'>
+                            {message.msg}
+                        </div>
+                        {/* {`${message.user}: ${message.msg}`} */}
+                    </div>
                 ))}
+            </div>
             <form className='chat-input-form' onSubmit={sendChat}>
                 <input
                     value={chatInput}
                     onChange={updateChatInput}
+                    placeholder={`Message #${room?.room_name}`}
                 />
                 <button type='submit'>Send</button>
             </form>
-        </div>
+        </>
+        // </div>
     )
 }
 

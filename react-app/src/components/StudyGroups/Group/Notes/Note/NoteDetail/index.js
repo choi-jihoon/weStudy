9c835fee02e6716172eb59e66a3a7bd2fc9febc9
jihoonchoi.94@gmail.com <jihoonchoi.94@gmail.com>
2022-02-16 13:51:05 -0800

    toast notification for editing note

--- react-app/src/components/StudyGroups/Group/Notes/Note/NoteDetail/index.js
+++ react-app/src/components/StudyGroups/Group/Notes/Note/NoteDetail/index.js
@@ -1,12 +1,16 @@
 import { useParams } from 'react-router-dom';
 import { useEffect, useState } from 'react';
 import { useSelector, useDispatch } from 'react-redux';
+
+import { toast } from "react-toastify";
+import "react-toastify/dist/ReactToastify.css";
+
 import { getNote, editNote } from '../../../../../../store/notes';
 import DeleteNote from '../DeleteNote';
 
-
 import './NoteDetail.css';
 
+toast.configure();
 
 const NoteDetail = () => {
     const { noteId } = useParams();
@@ -16,6 +20,13 @@ const NoteDetail = () => {
 
     const [title, setTitle] = useState('');
     const [noteText, setNoteText] = useState('');
+
+    const notify = () => {
+		toast(`Your edits have been saved!`, {
+			position: toast.POSITION.TOP_CENTER,
+			autoClose: 2000,
+		});
+	};
 
     const updateTitle = (e) => {
         setTitle(e.target.value);
@@ -27,6 +38,7 @@ const NoteDetail = () => {
 
     const handleSave = (e) => {
         dispatch(editNote(noteId, title, noteText))
+        notify();
     }
 
     useEffect(() => {

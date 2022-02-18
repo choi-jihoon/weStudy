77f60f06e063e4d7d32d281dca3f2f7cddb6856f
jihoonchoi.94@gmail.com <jihoonchoi.94@gmail.com>
2022-02-17 21:16:41 -0800

    splash

--- react-app/src/components/SplashPage/index.js
+++ react-app/src/components/SplashPage/index.js
@@ -9,7 +9,7 @@ const SplashPage = () => {
         <div className='splash-page-container'>
             <NavBar />
             <div className='title-container'>
-                <h4>Start studying together.</h4>
+                <h4>Studying's better together.</h4>
                 <div className='title-image'>
                     <h1>weStudy</h1>
                     <img src={logo} alt='logo'></img>

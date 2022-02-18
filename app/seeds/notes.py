62642380c9f2bab9390c03e2f5a5239110deda9e
jihoonchoi.94@gmail.com <jihoonchoi.94@gmail.com>
2022-02-18 07:53:49 -0800

    fixed seeder spacing

--- app/seeds/notes.py
+++ app/seeds/notes.py
@@ -5,18 +5,19 @@ def seed_notes():
         user_id = 1,
         group_id = 1,
         note_title = 'Python Lists and Tuples',
-        note_text = """Lists
-                    - mutable, ordered collections (like arrays in JavaScript)
-                    - defined using square brackets
+        note_text =
+        """Lists
+    - mutable, ordered collections (like arrays in JavaScript)
+    - defined using square brackets
 
-                    Tuples
-                    - immutable, ordered collections
-                    - defined using parentheses
-                    - sorted() will sort a tuple but return a list by default
+Tuples
+    - immutable, ordered collections
+    - defined using parentheses
+    - sorted() will sort a tuple but return a list by default
 
-                    Lists vs Tuples
-                    Tuples are: immutable, hashable, more memory efficient than a list
-                    Lists are: mutable and dynamic"""
+Lists vs Tuples
+    Tuples are: immutable, hashable, more memory efficient than a list
+    Lists are: mutable and dynamic"""
     )
 
     db.session.add(note1)

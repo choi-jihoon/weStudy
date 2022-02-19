45e9d298692efeaa2f7a6508366def0d2c168045
jihoonchoi.94@gmail.com <jihoonchoi.94@gmail.com>
2022-02-19 14:57:36 -0800

    fixed spacing

--- app/seeds/users.py
+++ app/seeds/users.py
@@ -3,10 +3,11 @@ from app.models import db, User
 
 # Adds a demo user, you can add other users here if you want
 def seed_users():
+    # image='https://fionacapstonebucket.s3.us-west-1.amazonaws.com/defaults/c203d7ca558d417b9aea8cd102ae32cf.jpg'
     demo = User(
         username='Demo', email='demo@aa.io', password='password', image='https://fionacapstonebucket.s3.amazonaws.com/defaults/52638a4b87654527b8fa25d13d81862a.png')
     fiona = User(
-        username='FionAH', email='fiona@aa.io', password='password', image='https://fionacapstonebucket.s3.us-west-1.amazonaws.com/defaults/c203d7ca558d417b9aea8cd102ae32cf.jpg')
+        username='FionAH', email='fiona@aa.io', password='password', image='https://fionacapstonebucket.s3.us-west-1.amazonaws.com/defaults/a2dc96297ad8452980d273e1f6571c35.jpg')
     denise = User(
         username='DAHNEEZ', email='denise@aa.io', password='password', image='https://fionacapstonebucket.s3.us-west-1.amazonaws.com/defaults/b43e30f40d54434e86339e63ecb88f3c.png')
     peter = User(

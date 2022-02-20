c25591217d3ed7c0fc0f9aab98f02d8484da2062
jihoonchoi.94@gmail.com <jihoonchoi.94@gmail.com>
2022-02-20 11:31:13 -0800

    min max length for room name

--- app/forms/room_form.py
+++ app/forms/room_form.py
@@ -1,9 +1,8 @@
-
 from flask_wtf import FlaskForm
 from wtforms import StringField, IntegerField
-from wtforms.validators import DataRequired, ValidationError
-from app.models import Room
+from wtforms.validators import DataRequired, Length
+
 
 class RoomForm(FlaskForm):
-    room_name = StringField('room_name', validators=[DataRequired()])
+    room_name = StringField('room_name', validators=[DataRequired(), Length(min=1, max=40, message="Room name must be less than 40 characters.")])
     group_id = IntegerField('group_id')

b39776391c0cdadb4bcb1f276bb10cb8dce40db9
jihoonchoi.94@gmail.com <jihoonchoi.94@gmail.com>
2022-02-20 12:20:25 -0800

    length validator on album form

--- app/forms/album_form.py
+++ app/forms/album_form.py
@@ -1,8 +1,8 @@
 from flask_wtf import FlaskForm
 from wtforms import IntegerField, StringField
-from wtforms.validators import DataRequired
+from wtforms.validators import DataRequired, Length
 
 class AlbumForm(FlaskForm):
     user_id = IntegerField('user_id', validators=[DataRequired()])
     group_id = IntegerField('group_id', validators=[DataRequired()])
-    title = StringField('title', validators=[DataRequired()])
+    title = StringField('title', validators=[DataRequired(), Length(min=1, max=40, message='Album title must be less than 40 characters.')])

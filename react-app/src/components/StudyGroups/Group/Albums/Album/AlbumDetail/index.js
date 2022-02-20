1a7ac1362a34d479f438cd3c45404a727f704340
jihoonchoi.94@gmail.com <jihoonchoi.94@gmail.com>
2022-02-20 13:29:51 -0800

    album detail view

--- react-app/src/components/StudyGroups/Group/Albums/Album/AlbumDetail/index.js
+++ react-app/src/components/StudyGroups/Group/Albums/Album/AlbumDetail/index.js
@@ -1,6 +1,24 @@
+import { useParams } from 'react-router-dom';
+import { useSelector, useDispatch } from 'react-redux';
+
+import './AlbumDetail.css';
+
+
 const AlbumDetail = () => {
+    const { albumId } = useParams();
+
+    const albums = useSelector(state => state.albums);
+    const album = albums.albums[albumId];
+
     return(
-        <h1>Album Detail!!</h1>
+        <div className='album-container'>
+            <div className='album-title-container'>
+                <h2 className='album-name'>{album.title}</h2>
+            </div>
+            <div className='album-images-container'>
+
+            </div>
+        </div>
     )
 }
 

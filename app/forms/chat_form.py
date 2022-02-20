f04484985883ef101bb119f52a21de625d35732b
jihoonchoi.94@gmail.com <jihoonchoi.94@gmail.com>
2022-02-20 11:32:59 -0800

    min max length for chat messages

--- app/forms/chat_form.py
+++ app/forms/chat_form.py
@@ -1,7 +1,7 @@
 from flask_wtf import FlaskForm
 from wtforms import StringField, IntegerField
-from wtforms.validators import DataRequired, ValidationError
+from wtforms.validators import DataRequired, Length
 
 class ChatForm(FlaskForm):
     room_id = IntegerField('room_id', validators=[DataRequired()])
-    message = StringField('message', validators=[DataRequired()])
+    message = StringField('message', validators=[DataRequired(), Length(min=1, max=500, message="Message must be less than 500 characters long.")])

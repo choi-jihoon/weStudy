67228dfa2377a27282d6ec8674c237919b3dee7b
jihoonchoi.94@gmail.com <jihoonchoi.94@gmail.com>
2022-02-20 13:39:57 -0800

    image reducer

--- app/forms/image_form.py
+++ app/forms/image_form.py
@@ -3,6 +3,5 @@ from wtforms import FileField, IntegerField
 from wtforms.validators import DataRequired
 
 class ImageForm(FlaskForm):
-    user_id = IntegerField('user_id', validators=[DataRequired()])
     album_id = IntegerField('album_id', validators=[DataRequired()])
     study_image = FileField('study_image')

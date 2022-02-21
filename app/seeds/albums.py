563d139892df0d9a08d757d7df2876abc0a2b0a3
jihoonchoi.94@gmail.com <jihoonchoi.94@gmail.com>
2022-02-21 09:52:50 -0800

    seeders

--- app/seeds/albums.py
+++ app/seeds/albums.py
@@ -13,8 +13,15 @@ def seed_albums():
         title='Programmer Humor'
     )
 
+    album3 = Album(
+        user_id=1,
+        group_id=1,
+        title='Pets'
+    )
+
     db.session.add(album1)
     db.session.add(album2)
+    db.session.add(album3)
 
 
 def undo_albums():
--- app/seeds/images.py
+++ app/seeds/images.py
@@ -37,12 +37,96 @@ def seed_images():
         study_image='https://fionacapstonebucket.s3.us-west-1.amazonaws.com/defaults/f4d8a3eac1be45c5a9a5b32fb8171eb4.jpg'
     )
 
+    pet1 = Image(
+        user_id=2,
+        album_id=3,
+        study_image='https://fionacapstonebucket.s3.us-west-1.amazonaws.com/defaults/9b952c8cc4114f21948333f0b19a1396.jpg'
+    )
+
+    pet2 = Image(
+        user_id=11,
+        album_id=3,
+        study_image='https://fionacapstonebucket.s3.us-west-1.amazonaws.com/defaults/c3f76e1652bc40b6a528e4db77599402.jpg'
+    )
+
+    pet3 = Image(
+        user_id=4,
+        album_id=3,
+        study_image='https://fionacapstonebucket.s3.us-west-1.amazonaws.com/defaults/20a5ac67b4d2404ba0cd02579a3548bc.jpg'
+    )
+
+    pet4 = Image(
+        user_id=11,
+        album_id=3,
+        study_image='https://fionacapstonebucket.s3.us-west-1.amazonaws.com/defaults/02145931647641a39326d7017a8b0ebc.jpg'
+    )
+
+    pet5 = Image(
+        user_id=10,
+        album_id=3,
+        study_image='https://fionacapstonebucket.s3.us-west-1.amazonaws.com/defaults/86516af8cd6f4193a09b3ee98ecd43ec.jpg'
+    )
+
+    pet6 = Image(
+        user_id=3,
+        album_id=3,
+        study_image='https://fionacapstonebucket.s3.us-west-1.amazonaws.com/defaults/f7cb27c68bf14bd3abd16aad89f08051.jpg'
+    )
+
+    pet7 = Image(
+        user_id=17,
+        album_id=3,
+        study_image='https://fionacapstonebucket.s3.us-west-1.amazonaws.com/defaults/a81705be4608460b9d431c8906934df4.png'
+    )
+
+    pet8 = Image(
+        user_id=3,
+        album_id=3,
+        study_image='https://fionacapstonebucket.s3.us-west-1.amazonaws.com/defaults/40e40006b7a84653a8a55feda8deef41.jpg'
+    )
+
+    pet9 = Image(
+        user_id=3,
+        album_id=3,
+        study_image='https://fionacapstonebucket.s3.us-west-1.amazonaws.com/defaults/873020b83b07491ebff532b5bfb4fc5a.jpg'
+    )
+
+    pet10 = Image(
+        user_id=11,
+        album_id=3,
+        study_image='https://fionacapstonebucket.s3.us-west-1.amazonaws.com/defaults/c96359ec6ebb427db95a686ff8cdd7a3.jpg'
+    )
+
+    pet11 = Image(
+        user_id=11,
+        album_id=3,
+        study_image='https://fionacapstonebucket.s3.us-west-1.amazonaws.com/defaults/212924a8e34845f2b2782086fcc0235c.jpg'
+    )
+
+    pet12 = Image(
+        user_id=2,
+        album_id=3,
+        study_image='https://fionacapstonebucket.s3.us-west-1.amazonaws.com/defaults/da7429271fcd4c07bd34a740f63aa719.jpg'
+    )
+
     db.session.add(we_study_db)
     db.session.add(conversion_chart)
     db.session.add(meme1)
     db.session.add(meme2)
     db.session.add(meme3)
     db.session.add(meme4)
+    db.session.add(pet1)
+    db.session.add(pet2)
+    db.session.add(pet3)
+    db.session.add(pet4)
+    db.session.add(pet5)
+    db.session.add(pet6)
+    db.session.add(pet7)
+    db.session.add(pet8)
+    db.session.add(pet9)
+    db.session.add(pet10)
+    db.session.add(pet11)
+    db.session.add(pet12)
     db.session.commit()
 
 def undo_images():

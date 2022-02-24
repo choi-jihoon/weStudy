afc775847c9f97ad8312c20dedf27f0c153f5d64
jihoonchoi.94@gmail.com <jihoonchoi.94@gmail.com>
2022-02-23 19:43:20 -0800

    no events component

---
+++ react-app/src/components/Dashboard/StudyGroups/Group/Events/NoEvents/index.js
@@ -0,0 +1,9 @@
+const NoEvents = () => {
+    return (
+        <div className='no-events-container'>
+            There are no events scheduled!
+        </div>
+    )
+};
+
+export default NoEvents;
